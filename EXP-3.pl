93e5a876cf11e686d724deb4784e06e8ebb1acee
KIRA1551 <vanshgaming173@gmail.com>
2025-10-28 10:13:50 +0530

    Update EXP-3.pl

--- EXP-3.pl
+++ EXP-3.pl
@@ -1,107 +1,41 @@
-% A FLIGHT ROUTE PLANNER
-:- op(50, xfy, :).
+female(pam).
 
-flight(Place1, Place2, Day, Fnum, Deptime, Arrtime) :-
-    timetable(Place1, Place2, Flightlist),
-    member(Deptime / Arrtime / Fnum / Daylist, Flightlist),
-    flyday(Day, Daylist).
+female(liz).
 
-member(X,[X|_]).
-member(X,[_Y|L]):- member(X,L).
+female(pat).
 
-flyday(Day, Daylist) :-
-    member(Day, Daylist).
-flyday(Day, alldays) :-
-    member(Day, [mo,tu,we,th,fr,sa,su]).
+female(ann).
 
-% Direct flight
-route(P1, P2, Day, [P1-P2 : Fnum : Deptime]) :-
-    flight(P1, P2, Day, Fnum, Deptime, _).
+male(jim).
 
-% Indirect flight (one or more transfers)
-route(P1, P2, Day, [P1-P3 : Fnum1 : Dep1 | Route]) :-
-    route(P3, P2, Day, Route),
-    flight(P1, P3, Day, Fnum1, Dep1, Arr1),
-    deptime(Route, Dep2),
-    transfer(Arr1, Dep2).
+male(bob).
 
-deptime([P1-_P2:_Fnum:Dep|], Dep).
+male(tom).
 
-% transfer requires at least 40 minutes between arrival and next departure
-transfer(H1:M1, H2:M2) :-
-    60*(H2 - H1) + M2 - M1 >= 40.
+male(peter).
 
-% A FLIGHT DATABASE (cleaned up)
-timetable(edinburgh, london, [
-    9:40  / 10:50 / ba4733 / alldays,
-    13:40 / 14:50 / ba4773 / alldays,
-    19:40 / 20:50 / ba4833 / [mo,tu,we,th,fr,su]
-]).
+parent(pam,bob).
 
-timetable(london, edinburgh, [
-    9:40  / 10:50 / ba4732 / alldays,
-    11:40 / 12:50 / ba4752 / alldays,
-    18:40 / 19:50 / ba4822 / [mo,tu,we,th,fr]
-]).
+parent(tom,bob).
 
-timetable(london, ljubljana, [
-    13:20 / 16:20 / ju201 / [fr],
-    13:20 / 16:20 / ju213 / [su]
-]).
+parent(tom,liz).
 
-timetable(london, zurich, [
-    9:10  / 11:45 / ba614 / alldays,
-    14:45 / 17:20 / sr805 / alldays
-]).
+parent(bob,ann).
 
-timetable(london, milan, [
-    8:30  / 11:20 / ba510 / alldays,
-    11:00 / 13:50 / az459 / alldays
-]).
+parent(bob,pat).
 
-timetable(ljubljana, zurich, [
-    11:30 / 12:40 / ju322 / [tu,th]
-]).
+parent(pat,jim).
 
-timetable(ljubljana, london, [
-    11:10 / 12:20 / ju200 / [fr],
-    11:25 / 12:20 / ju212 / [su]
-]).
+parent(bob,peter).
 
-timetable(milan, london, [
-    9:10  / 10:00 / az458 / alldays,
-    12:20 / 13:10 / ba511 / alldays
-]).
+parent(peter,jim).
 
-timetable(milan, zurich, [
-    9:25  / 10:15 / sr621 / alldays,
-    12:45 / 13:35 / sr623 / alldays
-]).
+mother(X,Y):- parent(X,Y),female(X).
 
-timetable(zurich, ljubljana, [
-    13:30 / 14:40 / yu323 / [tu,th]
-]).
+father(X,Y):- parent(X,Y),male(X).
 
-timetable(zurich, london, [
-    9:00  / 9:40  / ba613 / [mo,tu,we,th,fr,sa],
-    16:10 / 16:55 / sr806 / [mo,tu,we,th,fr,su]
-]).
+haschild(X):- parent(X,_).
 
-timetable(zurich, milan, [
-    7:55  / 8:45  / sr620 / alldays
-]).
+sister(X,Y):- parent(Z,X),parent(Z,Y),female(X),X\==Y.
 
-% helpers for list ops
-add(X,L,[X|L]).
-
-del(X,[X|Tail],Tail).
-del(X,[Y|Tail],[Y|Tail1]) :- del(X,Tail,Tail1).
-
-
-# Right the below code in SWI-Prolog.
-
-cd('C:/Users/<your name>/OneDrive/Documents/Prolog').
-
-[flights].
-
-flight(london, zurich, mo, Fnum, Dep, Arr).
+brother(X,Y):-parent(Z,X),parent(Z,Y),male(X),X\==Y.
